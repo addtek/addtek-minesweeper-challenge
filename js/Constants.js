cd4f8ee839927045c2e50de25839cb39304e8a92
Augustine Addey <47563562+addtek@users.noreply.github.com>
2020-07-13 08:53:06 +0000

    Delete Constants.js

--- js/Constants.js
+++
@@ -1,8 +0,0 @@
-import { Dimensions } from 'react-native';
-
-export default Constants = {
-    MAX_WIDTH: Dimensions.get('screen').width,
-    MAX_HEIGHT: Dimensions.get('screen').height,
-    BOARD_SIZE: 10,
-    CELL_SIZE: 40
-}
